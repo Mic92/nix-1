3a7f2225da81f5d70c06e4531eac72f3443f1f61
Raito Bezarius <raito@lix.systems>
2024-05-10 19:54:42 +0200

    Print the first line of stdout of SSH in case of failure
    In case of failure to connect as can be seen in
    https://buildbot.lix.systems/#/builders/39/builds/1386/steps/1/logs/stdio
    
    It is difficult to understand what happened, if we enabled the talkative
    verbose level, we could learn about the first line SSH sent us.
    
    In practice, this is not workable, we can just make it warn all the
    time.
    
    Cherry-picked from: https://git.lix.systems/lix-project/lix/commit/8404a1f66d090c5d68d8c5e958316df7d286a1e2
    
    Signed-off-by: Raito Bezarius <raito@lix.systems>

--- src/libstore/ssh.cc
+++ src/libstore/ssh.cc
@@ -124,7 +124,7 @@ std::unique_ptr<SSHMaster::Connection> SSHMaster::startCommand(
         } catch (EndOfFile & e) { }
 
         if (reply != "started") {
-            printTalkative("SSH stdout first line: %s", reply);
+            warn("SSH to '%s' failed, stdout first line: '%s'", host, reply);
             throw Error("failed to start SSH connection to '%s'", host);
         }
     }
