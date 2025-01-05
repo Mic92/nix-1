e6ab521fb4d7167b61cf20eaca7e61324f5b1e67
Jade Lovelace <lix@jade.fyi>
2025-01-05 19:46:10 +0100

    implement CLICOLOR_FORCE environment variable
    This allows to force colors but not disable other features such as the
    progress bar. This is for instance useful in CI environments.
    
    Adapted from
    https://git.lix.systems/lix-project/lix/commit/378ec5fb0611e314511a7afc806664205846fc2e
    and others.

--- src/libmain/shared.cc
+++ src/libmain/shared.cc
@@ -6,6 +6,8 @@
 #include "nix/main/loggers.hh"
 #include "nix/main/progress-bar.hh"
 #include "nix/util/signals.hh"
+#include "nix/util/terminal.hh"
+
 
 #include <algorithm>
 #include <exception>
@@ -356,7 +358,7 @@ int handleExceptions(const std::string & programName, std::function<void()> fun)
 
 RunPager::RunPager()
 {
-    if (!isatty(STDOUT_FILENO))
+    if (!isOutputARealTerminal(StandardOutputStream::Stdout))
         return;
     char * pager = getenv("NIX_PAGER");
     if (!pager)
