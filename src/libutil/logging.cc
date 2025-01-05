2360bf24c51cd5e04d461d6f17281fbdc85125a3
Jade Lovelace <lix@jade.fyi>
2025-01-05 19:46:10 +0100

    implement CLICOLOR_FORCE environment variable
    This allows to force colors but not disable other features such as the
    progress bar. This is for instance useful in CI environments.
    
    Adapted from
    https://git.lix.systems/lix-project/lix/commit/378ec5fb0611e314511a7afc806664205846fc2e
    and others.

--- src/libutil/logging.cc
+++ src/libutil/logging.cc
@@ -69,7 +69,7 @@ public:
         : printBuildLogs(printBuildLogs)
     {
         systemd = getEnv("IN_SYSTEMD") == "1";
-        tty = isTTY();
+        tty = shouldANSI();
     }
 
     bool isVerbose() override {
