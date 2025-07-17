215a88be0f63d1567541c1e5bd3d2703c2d1b82f
Jörg Thalheim <joerg@thalheim.io>
2025-07-17 12:26:50 +0200

    Fix Windows header inclusions for clang-tidy
    Move windows-error.hh includes inside _WIN32 guards to prevent
    clang-tidy errors when analyzing these files on non-Windows platforms.

--- src/libutil/windows/windows-async-pipe.cc
+++ src/libutil/windows/windows-async-pipe.cc
@@ -1,7 +1,6 @@
-#include "nix/util/windows-async-pipe.hh"
-
 #ifdef _WIN32
-#include "nix/util/windows-error.hh"
+#  include "nix/util/windows-async-pipe.hh"
+#  include "nix/util/windows-error.hh"
 
 namespace nix::windows {
 
