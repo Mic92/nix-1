b4b9efc5b1a096a58d12652e5dd04e2dab663aef
Jörg Thalheim <joerg@thalheim.io>
2025-10-01 17:49:58 +0200

    add builtin:write-file

---
+++ src/libstore/builtins/write-file.cc
@@ -0,0 +1,31 @@
+#include "nix/store/builtins.hh"
+#include "nix/store/store-api.hh"
+#include "nix/util/file-system.hh"
+
+namespace nix {
+
+static void builtinWriteFile(const BuiltinBuilderContext & ctx)
+{
+    auto out = get(ctx.drv.outputs, "out");
+    if (!out)
+        throw Error("'builtin:write-file' requires an 'out' output");
+
+    auto storePath = ctx.outputs.at("out");
+
+    auto contentIt = ctx.drv.env.find("content");
+    if (contentIt == ctx.drv.env.end())
+        throw Error("'builtin:write-file' requires 'content' parameter");
+    auto content = contentIt->second;
+
+    writeFile(storePath, content);
+
+    auto executableIt = ctx.drv.env.find("executable");
+    if (executableIt != ctx.drv.env.end() && executableIt->second == "1") {
+        if (::chmod(storePath.c_str(), 0755) == -1)
+            throw SysError("making '%1%' executable", storePath);
+    }
+}
+
+static RegisterBuiltinBuilder registerWriteFile("write-file", builtinWriteFile);
+
+} // namespace nix
